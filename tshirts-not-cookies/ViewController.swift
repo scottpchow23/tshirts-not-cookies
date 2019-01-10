ac9d1d0cc8c7f3e01fe3ee83a36b971daf6dff52
Scott Chow <tehchowster@gmail.com>
2019-01-09 19:05:12 -0800

    Complete version of the app.

--- tshirts-not-cookies/ViewController.swift
+++ tshirts-not-cookies/ViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var countLabel: UILabel!
+
+    var count = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        print("View did load")
     }
 
+    @IBAction func tapButtonTUI(_ sender: Any) {
+        print("Button was tapped")
+        incrementCount()
 
+        self.view.backgroundColor = randomColor()
+    }
+
+    func randomColor() -> UIColor {
+        let red = CGFloat.random(in: 0...1)
+        let green = CGFloat.random(in: 0...1)
+        let blue = CGFloat.random(in: 0...1)
+        print("Created the color r:\(red * 255) b:\(blue * 255) g:\(green * 255)")
+
+        return UIColor(red: red, green: green, blue: blue, alpha: 1)
+    }
+
+    func incrementCount() {
+        count = count + 1
+        print("\(count) t-shirts")
+        countLabel.text = "\(count) t-shirts"
+    }
 }
 
